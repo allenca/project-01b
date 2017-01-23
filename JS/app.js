213cc12662d41e730ac699f27ac0c0bf8c675666
allenca <allen.ca.chan@gmail.com>
2017-01-23 06:43:55 -0500

    007
    File directoy organized.
    jQuery, owl carousel slider, javascipt added
    Products added/edited
    Add to cart button added
    Fixed image references
    Tried to fix various responsiveness errors
    Tried to troubleshoot jQuery for add to cart function

---
+++ JS/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1240:{
+            items:4
+        }
+    }
+})
+  $(".addtocart").on("click", function (){
+       var inputField = parseInt($("#cartQty").val()); 
+       })
+  //increase the counter per click on add to cart
+  
+  $(".addtocart").click(function(){ 
+   $("#cartQty").show();
+})
+  //shows the increase in counter per click on add to cart
+
+});
