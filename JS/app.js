096ce3e2ed49af0e953f7826c337499904099c95
allenca <allen.ca.chan@gmail.com>
2017-02-01 11:36:46 -0500

    012
    Fixed code for the counter

--- JS/app.js
+++ JS/app.js
@@ -14,16 +14,33 @@ $(document).ready(function(){
             items:4
         }
     }
-})
+  });
+
   $(".addtocart").on("click", function (){
-       var inputField = parseInt($("#cartQty").val()); 
-       })
+    // 1. check exsisting cart value
+    var inputField = parseInt($("#cartQty").val());
+    inputField = inputField + 1; 
+    // 2. add 1 to the value
+    $("#cartQty").val(inputField++);
+    // 3. put value back in input
+    $("#cartQty").show();
+  })
   //increase the counter per click on add to cart
-  
-  $(".addtocart").click(function(){ 
-   $("#cartQty").show();
-})
   //shows the increase in counter per click on add to cart
+
+  // $(".addtocart").on("click", function (){
+  //   // 1. check exsisting cart value
+  //   // grab and set node for input
+  //   var inputCheck = $("#cartQty");
+  //   console.log((5 > 0 ? "hello" : "bye")); 
+  //   var exsistingVal = ($(inputCheck).val() == "null" ? 0 : parseInt($(inputCheck).val()));
+
+  //   //inputField = inputField + 1; 
+  //   // 2. add 1 to the value
+  //   $(inputCheck).val(exsistingVal++);
+  //   $(inputCheck).show();
+  // })
+
 });
 
 
